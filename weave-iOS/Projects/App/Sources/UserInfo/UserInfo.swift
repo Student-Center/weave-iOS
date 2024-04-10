29ca40fdf4c20edc1dda0b712ecaeec285688645
Jisu Kim <jisu15.kim@gmail.com>
2024-04-10 21:19:18 +0900

    [Fix] AppView 및 Login Feature 구조 변경

--- weave-iOS/Projects/App/Sources/UserInfo/UserInfo.swift
+++ weave-iOS/Projects/App/Sources/UserInfo/UserInfo.swift
@@ -6,7 +6,15 @@
 //
 
 import Foundation
+import Services
 
 enum UserInfo {
     static var myInfo: MyUserInfoModel?
+    
+    static func updateUserInfo() async throws {
+        let endPoint = APIEndpoints.getMyUserInfo()
+        let provider = APIProvider()
+        let userInfo = try await provider.request(with: endPoint, showErrorAlert: false)
+        UserInfo.myInfo = userInfo.toDomain
+    }
 }
