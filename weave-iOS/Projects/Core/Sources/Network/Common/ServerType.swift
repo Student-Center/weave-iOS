48727a4db52cf96bd22e23bb4ed8c5cb8a77865f
Jisu Kim <108998071+jisu15-kim@users.noreply.github.com>
2024-04-21 21:38:34 +0900

    [Refactor] 서버 타입 분기 코드 리팩토링 (#64)

--- weave-iOS/Projects/Core/Sources/Network/Common/ServerType.swift
+++ weave-iOS/Projects/Core/Sources/Network/Common/ServerType.swift
@@ -7,15 +7,15 @@
 
 import Foundation
 
-enum ServerType {
-    case develop // db 개발, api 개발
-    case release // db 상용, api 상용
+enum ServerType: String {
+    case dev // db 개발, api 개발
+    case prod // db 상용, api 상용
     
     var baseURL: String {
         switch self {
-        case .develop:
+        case .dev:
             return SecretKey.developURL
-        case .release:
+        case .prod:
             return SecretKey.releaseURL
         }
     }
